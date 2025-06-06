ac6ddee9aae49bd497554b37f9e257b2f8d775f6
Muwanguzi William Miagadde <ssebbikamigadde@gmail.com>
2025-06-06 03:15:35 +0300

    a new commit with uodated email.

--- instagramClone/src/app/(tabs)/index.tsx
+++ instagramClone/src/app/(tabs)/index.tsx
@@ -1,10 +1,22 @@
+import feedPosts from "@/app/utils/data/data.json";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
+const post = feedPosts[0];
 export default function FeedScreen() {
+  console.log(post);
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Feeds</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+  },
+});
--- instagramClone/src/app/utils/types/types.ts
+++ instagramClone/src/app/utils/types/types.ts
@@ -1,3 +1,14 @@
 export type UserType = {
   id: string;
+  avatar_url: string;
+  image_url: string;
+  username: string;
+};
+
+export type FeedType = {
+  id: string;
+  image: string;
+  image_url: string;
+  caption: string;
+  user: UserType;
 };
